ee85fd93297e1c818e71c5d3cfb09a903734eccf
akpadia02 <akpadia02@gmail.com>
2024-10-04 20:04:02 +0530

    4/10/24

--- Arrays/SingleNumber.java
+++ Arrays/SingleNumber.java
@@ -20,6 +20,34 @@ class Solution {
 //O(N^2)=TC
 //O(N)=SC
 
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int n=nums.length;
+        if(n==1){
+            return nums[0];
+        }
+        for(int i=0;i<n;i++){
+            if(i==0){
+                if(nums[i]!=nums[i+1]){
+                    return nums[i];
+                }
+            }else if(i==n-1){
+                if(nums[i]!=nums[i-1]){
+                    return nums[i];
+                }
+            }else{
+                if(nums[i]!=nums[i-1] && nums[i]!=nums[i+1]){
+                    return nums[i];
+                }
+            }
+        }
+        return -1;
+    }
+    
+}
+
+//O(N)
+
 
 import java.util.HashMap;
 
---
+++ BinarySearch/leet540_SingleNumber.java
@@ -0,0 +1,72 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int n=nums.length;
+        if(n==1){
+            return nums[0];
+        }
+        if(nums[0]!=nums[1]){
+            return nums[0];
+        }
+        if(nums[n-1]!=nums[n-2]){
+            return nums[n-1];
+        }
+        int low=1,high=n-2;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]){
+                return nums[mid];
+            }
+            if ((mid % 2 == 0 && nums[mid] == nums[mid + 1]) || 
+                (mid % 2 == 1 && nums[mid] == nums[mid - 1])) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+    
+}
+
+
+//ArrayList
+import java.util.ArrayList;
+public class Solution
+{
+    public static int singleNonDuplicate(ArrayList<Integer> arr)
+    {
+        //    Write your code here.
+        int n=arr.size();
+        if (n == 1)
+            return arr.get(0);
+        if (!arr.get(0).equals(arr.get(1)))
+            return arr.get(0);
+        if (!arr.get(n - 1).equals(arr.get(n - 2)))
+            return arr.get(n - 1);
+
+        int low = 1, high = n - 2;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            // If arr[mid] is the single element:
+            if (!arr.get(mid).equals(arr.get(mid + 1)) && !arr.get(mid).equals(arr.get(mid - 1))) {
+                return arr.get(mid);
+            }
+
+            // We are in the left:
+            if ((mid % 2 == 1 && arr.get(mid).equals(arr.get(mid - 1)))
+                    || (mid % 2 == 0 && arr.get(mid).equals(arr.get(mid + 1)))) {
+                // Eliminate the left half:
+                low = mid + 1;
+            }
+            // We are in the right:
+            else {
+                // Eliminate the right half:
+                high = mid - 1;
+            }
+        }
+
+        // Dummy return statement:
+        return -1;
+    }
+}
