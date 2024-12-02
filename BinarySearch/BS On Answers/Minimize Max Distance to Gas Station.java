53aaabf8d4f15e69d353260a5bfbf1567776fc75
akpadia02 <akpadia02@gmail.com>
2024-12-02 23:00:09 +0530

    hey

---
+++ BinarySearch/BS On Answers/Minimize Max Distance to Gas Station.java
@@ -0,0 +1,79 @@
+//brute O(k*n) + O(n)
+public class Solution {
+    public static double MinimiseMaxDistance(int []arr, int k){
+        // Write your code here.
+        int n = arr.length; //size of array.
+        int[] howMany = new int[n - 1];
+
+        //Pick and place k gas stations:
+        for (int gasStations = 1; gasStations <= k; gasStations++) {
+            //Find the maximum section
+            //and insert the gas station:
+            double maxSection = -1;
+            int maxInd = -1;
+            for (int i = 0; i < n - 1; i++) {
+                double diff = arr[i + 1] - arr[i];
+                double sectionLength =
+                    diff / (double)(howMany[i] + 1);
+                if (sectionLength > maxSection) {
+                    maxSection = sectionLength;
+                    maxInd = i;
+                }
+            }
+            //insert the current gas station:
+            howMany[maxInd]++;
+        }
+
+        //Find the maximum distance i.e. the answer:
+        double maxAns = -1;
+        for (int i = 0; i < n - 1; i++) {
+            double diff = arr[i + 1] - arr[i];
+            double sectionLength =
+                diff / (double)(howMany[i] + 1);
+            maxAns = Math.max(maxAns, sectionLength);
+        }
+        return maxAns;
+    }
+}
+
+
+//BS O(n*log(Len)) + O(n)
+public class Solution {
+    public static int numberOfGasStationsRequired(double dist, int[] arr) {
+        int n = arr.length; // size of the array
+        int cnt = 0;
+        for (int i = 1; i < n; i++) {
+            int numberInBetween = (int)((arr[i] - arr[i - 1]) / dist);
+            if ((arr[i] - arr[i - 1]) == (dist * numberInBetween)) {
+                numberInBetween--;
+            }
+            cnt += numberInBetween;
+        }
+        return cnt;
+    }
+
+    public static double MinimiseMaxDistance(int []arr, int k){
+        int n = arr.length; // size of the array
+        double low = 0;
+        double high = 0;
+
+        //Find the maximum distance:
+        for (int i = 0; i < n - 1; i++) {
+            high = Math.max(high, (double)(arr[i + 1] - arr[i]));
+        }
+
+        //Apply Binary search:
+        double diff = 1e-6 ;
+        while (high - low > diff) {
+            double mid = (low + high) / (2.0);
+            int cnt = numberOfGasStationsRequired(mid, arr);
+            if (cnt > k) {
+                low = mid;
+            } else {
+                high = mid;
+            }
+        }
+        return high;
+    }
+
+}
